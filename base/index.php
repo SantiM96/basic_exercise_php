56ea189ebbd40e01012856032f36aa95e4374aea
SantiM96 <smt093@gmail.com>
2020-07-23 18:41:03 -0300

    upload the exercise to git

---
+++ base/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+  </head>
+  <body>
+
+    <div class="contenedor">
+      <h1>Aprendiendo PHP</h1>
+
+        <div class="contenido">
+            <?php
+
+                $variable = 10;
+                echo $variable;
+            
+            
+            
+            ?>
+              
+        </div>
+    </div>
+
+
+
+
+  </body>
+</html>
